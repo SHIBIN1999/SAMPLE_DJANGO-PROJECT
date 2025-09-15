ee18168a81dabae6e93acb9360ea9daff61e0f10
shibin1999 <shibinshahir7@gmail.com>
2025-09-15 16:21:22 +0530

    html add django template language in third:filter using {{variable|filers}}

--- firstproject/subapp/views.py
+++ firstproject/subapp/views.py
@@ -3,6 +3,6 @@ from django.http import HttpResponse
 # Create your views here.
 def create(request):
     s={'dictionary_key':[{'title':'the drak','summary':'very good story','year':2000,'success':True},
-       {'title':'the dream','summary':'very  story','year':2010,'success':True},
+       {'title':'the dream','summary':'very  story','success':True},
        {'title':'the drak','summary':'very good ','year':2015,'success':True}]}
     return render(request,'create.html',s)
