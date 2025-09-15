1c1e943ad1ce77f30fc1ec25cddf23c743e706c8
shibin1999 <shibinshahir7@gmail.com>
2025-09-15 15:40:59 +0530

    html value add and using django template language in variable tag

--- firstproject/subapp/views.py
+++ firstproject/subapp/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def create(request):
-    return render(request,'create.html')
+    s={'title':'the drak','summary':'very good story','year':1999}
+    return render(request,'create.html',s)
