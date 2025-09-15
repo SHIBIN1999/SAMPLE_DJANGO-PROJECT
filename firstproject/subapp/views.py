41e3d2807daa32835338a8b2a7fd3e320369dda0
shibin1999 <shibinshahir7@gmail.com>
2025-09-15 16:10:15 +0530

    html add django template language in second:tag in using for loop and large dictionary is created

--- firstproject/subapp/views.py
+++ firstproject/subapp/views.py
@@ -2,5 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def create(request):
-    s={'title':'the drak','summary':'very good story','year':1999,'success':False}
+    s={'dictionary_key':[{'title':'the drak','summary':'very good story','year':2000,'success':True},
+       {'title':'the dream','summary':'very  story','year':2010,'success':True},
+       {'title':'the drak','summary':'very good ','year':2015,'success':True}]}
     return render(request,'create.html',s)
