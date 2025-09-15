7260b30b20099f18c5957784feaa68d6b3827429
shibin1999 <shibinshahir7@gmail.com>
2025-09-15 19:35:42 +0530

    static file include lot of image

--- Urlproject/subapp/views.py
+++ Urlproject/subapp/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 def create(request):
-    return render(request,'create.html')
+    s={'distionary_key':[{'img':'1.jpeg'},
+                         {'img':'1.jpeg'},
+                         {'img':'1.jpeg'},]}
+    return render(request,'create.html',s)
 def list(request):
     return render(request,'list.html')
 def edit(request):
